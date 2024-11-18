d88fcf9b724a014b084058e2ca88c8f5166b266f
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2024-11-18 03:26:45 +0100

    AI now work

--- generate/ai.h
+++ generate/ai.h
@@ -11,7 +11,7 @@
 
 #line 1 "../src/ai.h2"
 
-#line 14 "../src/ai.h2"
+#line 13 "../src/ai.h2"
 class Ai;
     
 
@@ -24,7 +24,7 @@ class Ai;
 #line 4 "../src/ai.h2"
 auto createAi() -> void;
 
-#line 14 "../src/ai.h2"
+#line 13 "../src/ai.h2"
 class Ai: public Player {
 
     private: snn::StraightforwardNeuralNetwork neuralNetwork; 
@@ -33,25 +33,25 @@ class Ai: public Player {
 
     public: explicit Ai(cpp2::impl::in<Color> c);
 
-#line 30 "../src/ai.h2"
+#line 28 "../src/ai.h2"
     public: Ai(Ai const& that);
 
-#line 35 "../src/ai.h2"
+#line 33 "../src/ai.h2"
     private: [[nodiscard]] auto getGobanState(cpp2::impl::in<State<Stone>> state) const& -> std::vector<float>;
 
-#line 60 "../src/ai.h2"
+#line 58 "../src/ai.h2"
     private: [[nodiscard]] auto chooseBestMove(cpp2::impl::in<std::vector<float>> nn_output) const& -> Move;
 
-#line 85 "../src/ai.h2"
+#line 83 "../src/ai.h2"
     public: [[nodiscard]] auto getMove(Engine& engine) -> Move override;
 
-#line 98 "../src/ai.h2"
+#line 96 "../src/ai.h2"
     public: auto train() & -> void;
 
-#line 121 "../src/ai.h2"
+#line 119 "../src/ai.h2"
     public: auto save() & -> void;
 
-#line 124 "../src/ai.h2"
+#line 122 "../src/ai.h2"
 };
 
 
@@ -62,73 +62,72 @@ class Ai: public Player {
 #line 4 "../src/ai.h2"
 auto createAi() -> void{
     std::vector<snn::LayerModel> layers {snn::Input(361), 
-                                                snn::FullyConnected(100), 
-                                                snn::FullyConnected(361)}; 
-    auto optimizer {snn::StochasticGradientDescent(0.0001f, 0.0f)}; 
+                                            snn::FullyConnected(100), 
+                                            snn::FullyConnected(361)}; 
+    auto optimizer {snn::StochasticGradientDescent(1e-8f, 0.0f)}; 
     auto neuralNetwork {snn::StraightforwardNeuralNetwork(cpp2::move(layers), cpp2::move(optimizer))}; 
     CPP2_UFCS(saveAs)(cpp2::move(neuralNetwork), "./snn_models/first_model.snn");
 }
 
-#line 20 "../src/ai.h2"
+#line 19 "../src/ai.h2"
     Ai::Ai(cpp2::impl::in<Color> c)
         : Player{ c }
         , neuralNetwork{ snn::StraightforwardNeuralNetwork::loadFrom("./snn_models/first_model.snn") }
         , inputs{  }
         , moves{  }{
 
-#line 25 "../src/ai.h2"
+#line 24 "../src/ai.h2"
         CPP2_UFCS(reserve)(inputs, 300);
         CPP2_UFCS(reserve)(moves, 300);
-        setNextMessage("AI loaded.");
     }
 
-#line 30 "../src/ai.h2"
+#line 28 "../src/ai.h2"
     Ai::Ai(Ai const& that)
         : Player{ that }
         , neuralNetwork{ that.neuralNetwork }
         , inputs{ that.inputs }
         , moves{ that.moves }{
 
-#line 33 "../src/ai.h2"
+#line 31 "../src/ai.h2"
     }
 
-#line 35 "../src/ai.h2"
+#line 33 "../src/ai.h2"
     [[nodiscard]] auto Ai::getGobanState(cpp2::impl::in<State<Stone>> state) const& -> std::vector<float>{
         std::vector<float> vec {}; 
         CPP2_UFCS(reserve)(vec, 361);
 {
 cpp2::i8 col{0};
 
-#line 39 "../src/ai.h2"
+#line 37 "../src/ai.h2"
         for( ; cpp2::impl::cmp_less(col,CPP2_UFCS(ssize)(state)); 
         ++col ) 
         {
 {
 cpp2::i8 row{0};
 
-#line 43 "../src/ai.h2"
+#line 41 "../src/ai.h2"
             for( ; cpp2::impl::cmp_less(row,CPP2_UFCS(ssize)(CPP2_ASSERT_IN_BOUNDS(state, col))); 
             ++row ) 
             {
                 if (CPP2_ASSERT_IN_BOUNDS(CPP2_ASSERT_IN_BOUNDS(state, col), row).color == color) {
-                    CPP2_UFCS(push_back)(vec, 0.5);
+                    CPP2_UFCS(push_back)(vec, 1.0);
                 }
-                if (CPP2_ASSERT_IN_BOUNDS(CPP2_ASSERT_IN_BOUNDS(state, col), row).color == Color::None) {
-                    CPP2_UFCS(push_back)(vec, 0.5);
+                else {if (CPP2_ASSERT_IN_BOUNDS(CPP2_ASSERT_IN_BOUNDS(state, col), row).color == Color::None) {
+                    CPP2_UFCS(push_back)(vec, 0.0);
                 }
                 else {
-                    CPP2_UFCS(push_back)(vec, 0.5);
-                }
+                    CPP2_UFCS(push_back)(vec, -1.0);
+                }}
             }
 }
-#line 56 "../src/ai.h2"
+#line 54 "../src/ai.h2"
         }
 }
-#line 57 "../src/ai.h2"
+#line 55 "../src/ai.h2"
         return vec; 
     }
 
-#line 60 "../src/ai.h2"
+#line 58 "../src/ai.h2"
     [[nodiscard]] auto Ai::chooseBestMove(cpp2::impl::in<std::vector<float>> nn_output) const& -> Move{
         cpp2::i16 index {0}; 
         float max {0}; 
@@ -137,14 +136,14 @@ cpp2::i8 row{0};
 {
 cpp2::i8 col{0};
 
-#line 66 "../src/ai.h2"
+#line 64 "../src/ai.h2"
         for( ; cpp2::impl::cmp_less(col,19); 
         ++col ) 
         {
 {
 cpp2::i8 row{0};
 
-#line 70 "../src/ai.h2"
+#line 68 "../src/ai.h2"
             for( ; cpp2::impl::cmp_less(row,19); 
             ++row ) 
             {
@@ -156,15 +155,15 @@ cpp2::i8 row{0};
                 ++index;
             }
 }
-#line 80 "../src/ai.h2"
+#line 78 "../src/ai.h2"
         }
 }
-#line 81 "../src/ai.h2"
+#line 79 "../src/ai.h2"
         auto m {Move(color, cpp2::move(max_col), cpp2::move(max_row))}; 
         return m; 
     }
 
-#line 85 "../src/ai.h2"
+#line 83 "../src/ai.h2"
     [[nodiscard]] auto Ai::getMove(Engine& engine) -> Move{
         auto input {getGobanState(engine.goban.state)}; 
         auto output {CPP2_UFCS(computeOutput)(neuralNetwork, input)}; 
@@ -178,7 +177,7 @@ cpp2::i8 row{0};
         return m; 
     }
 
-#line 98 "../src/ai.h2"
+#line 96 "../src/ai.h2"
     auto Ai::train() & -> void{
         float expectedValue {-1.0}; 
         if (hasWon) {// CPP2 workaround: Conditional operator not yet supported.
@@ -187,7 +186,7 @@ cpp2::i8 row{0};
 {
 cpp2::i16 i{0};
 
-#line 104 "../src/ai.h2"
+#line 102 "../src/ai.h2"
         for( ; cpp2::impl::cmp_less(i,CPP2_UFCS(ssize)(inputs)); 
         ++i ) 
         {
@@ -195,7 +194,7 @@ cpp2::i16 i{0};
 {
 cpp2::i16 j{0};
 
-#line 109 "../src/ai.h2"
+#line 107 "../src/ai.h2"
             for( ; cpp2::impl::cmp_less(j,361); 
             ++j ) 
             {
@@ -204,15 +203,15 @@ cpp2::i16 j{0};
                 }
             }
 }
-#line 116 "../src/ai.h2"
+#line 114 "../src/ai.h2"
             CPP2_ASSERT_IN_BOUNDS(expected_output, CPP2_ASSERT_IN_BOUNDS(moves, i)) = expectedValue;
             CPP2_UFCS(trainOnce)(neuralNetwork, CPP2_ASSERT_IN_BOUNDS(inputs, i), cpp2::move(expected_output));
         }
 }
-#line 119 "../src/ai.h2"
+#line 117 "../src/ai.h2"
     }
 
-#line 121 "../src/ai.h2"
+#line 119 "../src/ai.h2"
     auto Ai::save() & -> void{
         CPP2_UFCS(saveAs)(neuralNetwork, "./snn_models/first_model.snn");
     }
--- generate/main.cpp
+++ generate/main.cpp
@@ -21,14 +21,26 @@
 #include "ai.h"
 
 #line 9 "../src/main.cpp2"
-[[nodiscard]] auto main() -> int;
+[[nodiscard]] auto cpp2_main() -> int;
+#line 101 "../src/main.cpp2"
+
+
+int main() {
+    try { // CPP2 workaround: Try catch not yet supported.
+        return cpp2_main();
+    } catch (const std::exception& e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
+
 
 //=== Cpp2 function definitions =================================================
 
 #line 1 "../src/main.cpp2"
 
 #line 9 "../src/main.cpp2"
-[[nodiscard]] auto main() -> int{
+[[nodiscard]] auto cpp2_main() -> int{
     SetConsoleOutputCP(CP_UTF8);
     bool exit {false}; 
     do {
@@ -50,17 +62,11 @@
             else if (cpp2::impl::is(_expr, 5)) { if constexpr( requires{[]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; CPP2_UFCS_TEMPLATE(play<false>)(cpp2::move(game));waitInput();return false; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; CPP2_UFCS_TEMPLATE(play<false>)(cpp2::move(game));waitInput();return false; }())),bool> ) return []() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Random>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; CPP2_UFCS_TEMPLATE(play<false>)(cpp2::move(game));waitInput();return false; }(); else return bool{}; else return bool{}; }
 
 #line 65 "../src/main.cpp2"
-            else if (cpp2::impl::is(_expr, 6)) { if constexpr( requires{[]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);++i;}return false; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);++i;}return false; }())),bool> ) return []() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(cpp2::move(player1), cpp2::move(player2))}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);++i;}return false; }(); else return bool{}; else return bool{}; }
-
-#line 74 "../src/main.cpp2"
-                    //player1*.train();
-                    //player2*.train();
-                    /*if i % 20 == 19 {
-                        player1.save();
-                    }*/
+            else if (cpp2::impl::is(_expr, 6)) { if constexpr( requires{[]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(player1, player2)}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player1)));CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player2)));if (i % 20 == 19) {CPP2_UFCS(save)((*cpp2::impl::assert_not_null(player1)));}++i;}return false; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(player1, player2)}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player1)));CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player2)));if (i % 20 == 19) {CPP2_UFCS(save)((*cpp2::impl::assert_not_null(player1)));}++i;}return false; }())),bool> ) return []() -> bool{clear();auto player1 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::Black)}; auto player2 {CPP2_UFCS_TEMPLATE(cpp2_new<Ai>)(cpp2::shared, Color::White)}; auto game {Game(player1, player2)}; cpp2::i32 i {0}; while( true ) {CPP2_UFCS_TEMPLATE(play<false>)(game);CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player1)));CPP2_UFCS(train)((*cpp2::impl::assert_not_null(player2)));if (i % 20 == 19) {CPP2_UFCS(save)((*cpp2::impl::assert_not_null(player1)));}++i;}return false; }(); else return bool{}; else return bool{}; }
 
 #line 83 "../src/main.cpp2"
-            else if (cpp2::impl::is(_expr, 9)) { if constexpr( requires{[]() -> bool{clear();createAi();setNextMessage("AI created.");return false; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{clear();createAi();setNextMessage("AI created.");return false; }())),bool> ) return []() -> bool{clear();createAi();setNextMessage("AI created.");return false; }(); else return bool{}; else return bool{}; }
+            else if (cpp2::impl::is(_expr, 9)) { if constexpr( requires{[]() -> bool{createAi();setNextMessage("AI created.");return false; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{createAi();setNextMessage("AI created.");return false; }())),bool> ) return []() -> bool{createAi();setNextMessage("AI created.");return false; }(); else return bool{}; else return bool{}; }
+                //clear();
 
 #line 89 "../src/main.cpp2"
            else if (cpp2::impl::is(_expr, 0)) { if constexpr( requires{[]() -> bool{clear("Bye.");return true; }();} ) if constexpr( std::is_convertible_v<CPP2_TYPEOF(([]() -> bool{clear("Bye.");return true; }())),bool> ) return []() -> bool{clear("Bye.");return true; }(); else return bool{}; else return bool{}; }
